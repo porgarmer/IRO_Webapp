dd7ee8968d60a6b7ab9ce7f1a4972eeb84a9c20b
rono <132464339+porgarmer@users.noreply.github.com>
2025-01-18 13:07:12 +0800

    navbar and footer. home partial

--- shelters/urls.py
+++ shelters/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.shelters_page, name="shelters_page"), 
+    # path("", views.shelters_page, name="shelters_page"), 
 ] 
