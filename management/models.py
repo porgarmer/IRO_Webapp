b1dda72df2a8e9af1b2ef0b1974e44621e4a76ce
rono <132464339+porgarmer@users.noreply.github.com>
2025-01-24 16:04:59 +0800

    added newsarticle and newsarticlecategory models

--- management/models.py
+++ management/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from ckeditor.fields import RichTextField  
-
+from datetime import datetime
 
 # Create your models here.
 class HomePage(models.Model):
@@ -16,5 +16,13 @@ class HomePage(models.Model):
     about_us_section_content = RichTextField(blank=True, null=True)
 
     
+class NewsArticleCategory(models.Model):
+    name = models.CharField(max_length=50)
+
 class NewsArticle(models.Model):
-    pass
+    photo = models.ImageField(null=True, blank=True, upload_to='news&articles/')
+    # author =  # to be added
+    date_published = models.DateField(auto_now_add=False, auto_now=False, default=datetime.today, null=True)
+    category = models.ForeignKey(NewsArticleCategory, null=True, blank=True, related_name='news_article', on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=255)
+    content = RichTextField(blank=True, null=True)
