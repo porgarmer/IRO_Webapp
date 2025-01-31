ee4a8ef96dec45aa1e0c782c835ebc0323809948
rono <132464339+porgarmer@users.noreply.github.com>
2025-01-31 22:57:08 +0800

    fix ckeditor not working

--- IRO_Webapp/settings.py
+++ IRO_Webapp/settings.py
@@ -163,6 +163,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
